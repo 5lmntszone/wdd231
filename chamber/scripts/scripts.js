0860f4b8c56b723ff1f7c92123b4c161a35f7771
Barbara <5elementszone@gmail.com>
2025-05-24 20:31:15 +0200

    added dark mode

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -46,3 +46,19 @@ const businesses = [
     container.appendChild(card);
   });
   
+const toggle = document.getElementById('darkToggle');
+const body = document.body;
+
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-mode');
+}
+
+toggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+
+  if (body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
