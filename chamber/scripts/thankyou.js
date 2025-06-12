dda715f577915aa6e2efe51bf153d6598e4c94ae
Barbara <5elementszone@gmail.com>
2025-06-12 22:14:06 +0200

    added thankyou page

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,13 @@
+const getString = window.location.search;
+
+const userInfo = new URLSearchParams(getString);
+
+document.querySelector('#results').innerHTML = `
+    <p>We're excited to welcome you, <strong>${userInfo.get('first')} ${userInfo.get('last')}</strong>.</p>
+    <p>Your organizational title: <strong>${userInfo.get('orgTitle')}</strong></p>
+    <p>Business Name: <strong>${userInfo.get('businessName')}</strong></p>
+    <p>We’ve sent a confirmation to <strong>${userInfo.get('email')}</strong>.</p>
+    <p>Form submitted on: <strong>${new Date(userInfo.get('timestamp')).toLocaleString()}</strong></p>
+    <p>We’ll be in touch soon with membership benefits and upcoming events.</p>
+    <a href="index.html" class="return-home-button">Return to Home</a>
+`;
