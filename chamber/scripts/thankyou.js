440f99a465e252ecb98d991af683acece9995fbc
Barbara <5elementszone@gmail.com>
2025-06-12 22:52:15 +0200

    added phone num

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -5,6 +5,7 @@ const userInfo = new URLSearchParams(getString);
 document.querySelector('#results').innerHTML = `
     <p>We're excited to welcome you, <strong>${userInfo.get('first')} ${userInfo.get('last')}</strong>.</p>
     <p>Your organizational title: <strong>${userInfo.get('orgTitle')}</strong></p>
+    <p>Your phone number: <strong>${userInfo.get('phone')}</strong></p>
     <p>Business Name: <strong>${userInfo.get('businessName')}</strong></p>
     <p>We’ve sent a confirmation to <strong>${userInfo.get('email')}</strong>.</p>
     <p>Form submitted on: <strong>${new Date(userInfo.get('timestamp')).toLocaleString()}</strong></p>
