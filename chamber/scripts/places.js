76c78362cbdd09b799f4d1e3e9daf15631c90680
Barbara <5elementszone@gmail.com>
2025-06-16 21:50:13 +0200

    added places cards

---
+++ chamber/scripts/places.js
@@ -0,0 +1,17 @@
+import {places} from '../data/places.mjs'
+
+const container = document.getElementById('allplaces');
+
+places.forEach(place => {
+    const card = document.createElement('div');
+
+    card.innerHTML = `
+    <h2>${place.name}</h2>
+    <img src="${place.img}" alt="${place.name}" width="300" height="200">
+    <p>${place.description}</p>
+    <address>${place.address}</address>
+    <button class="learn-more">Learn More</button>
+  `;
+
+  container.appendChild(card);
+});
