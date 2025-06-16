fa149fd59099e8524aa5e725491bc52ca0eaee8e
Barbara <5elementszone@gmail.com>
2025-06-16 22:29:06 +0200

    fixed layout

--- chamber/data/places.mjs
+++ chamber/data/places.mjs
@@ -3,49 +3,49 @@ export const places = [
       "name": "Jet d’Eau",
       "address": "Promenade du Quai Gustave-Ador, Geneva",
       "description": "This iconic 140 m-high fountain is Geneva’s signature landmark.",
-      "img": "https://traveldigg.com/jet-deau-fountain-the-icon-of-geneva/"
+      "img": '../images/jet_deau-1.webp'
     },
     {
       "name": "St Peter’s Cathedral",
       "address": "Cours St‑Pierre 6, 1204 Geneva",
       "description": "12th‑century cathedral with archaeological crypts and panoramic views from its towers.",
-      "img": "https://cdn.historyhit.com/wp-content/uploads/2018/12/St-Peters-Cathedral-Geneva.jpg"
+      "img": "../images/stpeter_cathedral-1.webp"
     },
     {
       "name": "Flower Clock (L’horloge Fleurie)",
       "address": "Promenade du Lac / Jardin Anglais, Geneva",
       "description": "Seasonal floral clock that celebrates Geneva’s watchmaking heritage.",
-      "img": "https://depositphotos.com/300102646/flower-clock-in-geneva-switzerland.html"
+      "img": "../images/flower_clock.webp"
     },
     {
       "name": "Parc des Bastions",
       "address": "Parc des Bastions, Geneva",
       "description": "Historic park home to the Reformation Wall and giant outdoor chessboards.",
-      "img": "https://www.visionsoftravel.org/parc-des-bastions-geneva-university-swatch-museum/"
+      "img": "../images/parc_des_bastions-1.webp"
     },
     {
       "name": "Patek Philippe Museum",
       "address": "Rue des Vieux‑Grenadiers 7, 1205 Geneva",
       "description": "World-class watch museum showcasing centuries of Swiss craftsmanship.",
-      "img": "https://www.centurion-magazine.com/sites/default/files/styles/feature_large/public/Patek%20Philippe%20Museum.jpg"
+      "img": "../images/patek_philippe_museum.webp"
     },
     {
       "name": "CERN & Globe of Science and Innovation",
       "address": "385 route de Meyrin, 1217 Meyrin, Geneva",
       "description": "Explore the world’s largest particle physics lab and its interactive science globe.",
-      "img": "https://example.com/images/cern-globe.jpg"
+      "img": "../images/ghlobe_of_science_and_innovation.webp"
     },
     {
       "name": "Musée Ariana",
       "address": "Avenue de la Paix 10, 1202 Geneva",
       "description": "Swiss museum of ceramics and glass, with over 20,000 works spanning 1,200 years.",
-      "img": "https://www.geneve.com/wp-content/uploads/2019/07/musee-ariana.jpg"
+      "img": "../images/musée_ariana-1.webp"
     },
     {
       "name": "Grand Théâtre de Genève",
       "address": "Place Neuve, 1204 Geneva",
       "description": "Majestic opera house presenting opera, ballet, and concerts since 1876.",
-      "img": "https://example.com/images/grand-theatre-geneva.jpg"
+      "img": "../images/grand_théâtre_de_genève.webp"
     }
   ]
   
